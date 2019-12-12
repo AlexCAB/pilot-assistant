d531d1f1f097e802d83b09787a9443ded24ca594
olsirotk <oleksii.sirotkin@qvantel.com>
2019-12-12 21:55:43 +0200

    0.0.0#008 Added logo

--- RaspberryPi/App/pa/com.py
+++ RaspberryPi/App/pa/com.py
@@ -14,26 +14,6 @@ __/_/     /_/  /_/  \____/\__/      /_/  |_/____/ /____/ /_/  /____/ \__/ \__,_/
 
 author: CAB
 website: github.com/alexcab
-created: 2019-11-11
+created: 2019-12-12
 """
 
-from enum import Enum
-
-
-class DashboardMode(Enum):
-    race = 1
-    street = 2
-
-
-class DashboardLevel(Enum):
-    inactive = 0
-    ok = 1
-    warning = 2
-    dangerous = 3
-
-
-class TurnIndication(Enum):
-    none = 0
-    left = 1
-    right = 2
-    both = 3
